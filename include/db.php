e7ef5eff744a1f8bb1921db9fb83ee0d2570afb2
adbrsln <adbroslan@gmail.com>
2016-06-18 02:55:12 +0800

    Delete db.php

--- include/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-//error_reporting(0);
-
-//$connect = mysqli_connect("localhost", "root", "","klkm") or die (mysqli_error());
- $connect = mysqli_connect('mysql.hostinger.my','u991240415_user','4vItQinFyokMB','u991240415_test') or die (mysqli_error($connect));
-
-?>
