a4fdfe21b61f9ee245d342ae13b79a5f1948715e
adbrsln <adbroslan@gmail.com>
2016-06-18 02:53:36 +0800

    remove db

--- include/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-//error_reporting(0);
-
-//$connect = mysqli_connect("localhost", "root", "","klkm") or die (mysqli_error());
- $connect = mysqli_connect('hostname','username','password','dbname') or die (mysqli_error($connect));
-
-?>
